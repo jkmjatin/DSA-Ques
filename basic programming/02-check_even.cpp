ec6beb3740cefce0c824c3be338e42f560cec41f
jkmjatin <jatinmehtagaya@gmail.com>
2025-08-19 01:11:18 +0530

    Add 02-check_even.cpp

---
+++ basic programming/02-check_even.cpp
@@ -0,0 +1,22 @@
+// check if a given number is even or odd
+
+// given number: 33
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+
+    num = 33; //given
+
+    if (num%2 == 0){
+        cout << "Given number is even" << endl;
+    }
+    else{
+        cout << "Given number is odd" << endl;
+    }
+
+    return 0;
+}
