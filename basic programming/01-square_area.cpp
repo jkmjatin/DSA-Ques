e76412bba5bcc10974b297a7348a1bfbec3c736d
jkmjatin <jatinmehtagaya@gmail.com>
2025-08-19 00:45:15 +0530

    Add 01-square_area.cpp

---
+++ basic programming/01-square_area.cpp
@@ -0,0 +1,19 @@
+// objective: calculate the area of a square
+
+// math formula: side * side
+// given side: 10 cms
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int side, area;
+
+    side = 10; //given
+    area = side * side;
+
+    cout << area << "cms sq." << endl;
+
+    return 0;
+}
